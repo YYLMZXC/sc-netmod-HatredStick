ad662ea2a060673c919559dd69362686e988a33f
YYLMZXC <YYLMZXC@163.com>
2025-01-08 13:24:51 +0800

    发布2.0

---
+++ res-Content/cr/HatredStick.cr
@@ -0,0 +1,12 @@
+<HatredStick>
+  <Recipes>
+		<Recipe Result="HatredStickBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="3" a="planks" b="ironingot" Description="合成仇恨之棍，要求等级lv3">
+		"ab"
+		"a"
+		</Recipe>
+		<Recipe Result="HatredStickBlock:1" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="3" a="planks" b="ironingot" Description="合成仇恨之棍，要求等级lv3">
+		"a"
+		"ab"
+		</Recipe>
+  </Recipes>
+</HatredStick>
